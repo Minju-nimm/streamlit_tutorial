7226bef45fc57b08cfb70e856e76052b55beb2d6
Minju-nimm <minjunimm@gmail.com>
2023-02-03 11:33:41 +0900

    my first streamlit app

---
+++ first_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.title("First_APP")
+    df=pd.Dataframe({
+        "first_col":[1,2,3,4],
+        "second_col":[100,20,30,40]
+        
+    })
