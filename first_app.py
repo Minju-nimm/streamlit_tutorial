12c5b000b97b86abc2553d59e2ec229b06535170
Minju-nimm <minjunimm@gmail.com>
2023-02-03 13:06:35 +0900

    my first streamlit app

--- first_app.py
+++ first_app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import pandas as pd
+# pip install pandas
+# conda install pandas
 
 def main():
     st.title("First_APP")
-    df=pd.Dataframe({
-        "first_col":[1,2,3,4],
-        "second_col":[100,20,30,40]
-        
+    df = pd.DataFrame({
+        "first_col":[1, 2, 3, 4],
+        "second_col":[10, 20, 30, 40]
     })
     st.write(df)
 
-if __name__=="__main__":
-    main()    
+if __name__ == "__main__":
+    main()
